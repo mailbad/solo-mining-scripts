c398629542adccfe7a70272ed61f7be8151c78fd
zert <mailbad@163.com>
2022-03-09 13:53:05 +0800

    fix uninstall

--- scripts/main.sh
+++ scripts/main.sh
@@ -86,9 +86,11 @@ function phala_scripts_ps_container() {
 }
 
 function phala_scripts_stop_container() {
-  phala_scripts_check_envf
-  phala_scripts_check_ymlf
-  local _container_name=$(awk -F':' '/container_name/ {print $NF}' ${phala_scripts_docker_ymlf}|grep "\-${1}$")
+  [ "$1" == "uninstall" ] || {
+    phala_scripts_check_envf
+    phala_scripts_check_ymlf
+  }
+  local _container_name=$(awk -F':' '/container_name/ {print $NF}' ${phala_scripts_docker_ymlf} 2>/dev/null|grep "\-${1}$")
   if [ -z "$1" ];then
     phala_scripts_utils_docker stop
     phala_scripts_utils_docker rm -f
@@ -191,7 +193,8 @@ function phala_scripts_case() {
       phala_scripts_ps_container
     ;;
     uninstall)
-      phala_scripts_stop_logs stop > /dev/null 2>&1
+      # phala_scripts_stop_logs stop > /dev/null 2>&1
+      phala_scripts_stop_container $1 > /dev/null 2>&1
       phala_scripts_uninstall
     ;;
     sgx-test)
